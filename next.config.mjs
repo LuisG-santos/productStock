8928c0dd34cc040806281e879ecf8e62f992dd12
Luis Gustavo <luisgustavo.nunes@icloud.com>
2026-01-28 14:50:58 -0300

    feat(dev): add analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+const withBundleAnalyzer = (await import('@next/bundle-analyzer')).default({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+export default withBundleAnalyzer(nextConfig);
