285fb8ba311eba4fd92f288a970ae5aabb2910d7
Luis Gustavo <luisgustavo.nunes@icloud.com>
2026-02-03 12:37:45 -0300

    feat(products): add actions on products table

--- app/products/_components/table-colums.tsx
+++ app/products/_components/table-colums.tsx
@@ -4,6 +4,23 @@ import { products } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 import { Badge } from "@/app/_components/ui/badge";
 import { FaCircle } from "react-icons/fa";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import { Button } from "@/app/_components/ui/button";
+import {
+  ClipboardCopyIcon,
+  MoreHorizontalIcon,
+  PencilIcon,
+  TrashIcon,
+} from "lucide-react";
+import {
+  DropdownMenuItem,
+} from "@/app/_components/ui/dropdown-menu";
 
 const translateStatus = (status: string) => {
   switch (status) {
@@ -45,6 +62,45 @@ export const productsTableCol: ColumnDef<products>[] = [
     cell: ({ row }) => {
       const status = row.getValue("status") as string;
       return translateStatus(status);
+    },
+  },
+  {
+    accessorKey: "actions",
+    header: "Ações",
+    cell: (row) => {
+      const product = row.row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost">
+              <MoreHorizontalIcon size={16} />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="bg-white rounded-lg dark:bg-zinc-900 hover:bg-gray-700">
+
+            <DropdownMenuItem
+              className="gap-1.5"
+              onClick={() => navigator.clipboard.writeText(product.id)}
+            >
+              <ClipboardCopyIcon size={16} />
+              Copiar ID
+            </DropdownMenuItem>
+
+            <DropdownMenuItem className="gap-1.5">
+              <PencilIcon size={16}/>
+              Editar
+              </DropdownMenuItem>
+            <DropdownMenuSeparator />
+
+            <DropdownMenuItem className="gap-1.5">
+              <TrashIcon size={16}/>
+              Deletar
+              </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
     },
   },
 ];
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,20 +1,16 @@
+"use server"
 import { DataTable } from "../_components/ui/data-table";
 import { productsTableCol } from "./_components/table-colums";
 import CreateProductsButton from "./_components/createProductButton";
-
-export const dynamic = "force-dynamic";
+import { getProducts } from "../_data-accsess/products/getProducts";
 
 const ProductsPage = async () => {
-  const response = await fetch("http://localhost:3000/api/products", {
-    method: "GET",
-    next: { revalidate: 5 },
-  });
-  const products = await response.json();
+ const products = await getProducts();
 
   return (
     <div className="h-full space-y-8 rounded-xl bg-white p-5 dark:bg-black">
       <div className="flex items-center justify-between">
-        <div className="space-y-1">
+        <div className="space-y-1 flex flex-col">
           <span className="text-xs font-semibold text-slate-500">
             Gestão de produtos
           </span>
